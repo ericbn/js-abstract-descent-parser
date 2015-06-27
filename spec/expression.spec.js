17570176c7cfc519d96b7b13951d2fa21f256bb1
Eric Nielsen <eric@amalgamar.com.br>
2015-06-27 18:45:40 -0300

    Updated index.html and expression.spec.js

--- spec/expression.spec.js
+++ spec/expression.spec.js
@@ -65,5 +65,60 @@ describe("Expression", function() {
     it("should parse fully parenthesized", function() {
       expect(Expression.parse("(5-(2*3))").result).toBe(-1);
     });
+    xit("should parse exponentiation", function() {
+      expect(Expression.parse("2*2^8").result).toBe(512);
+    });
+    xit("should parse negation", function() {
+      expect(Expression.parse("2^-(2*-2)+-5").result).toBe(11);
+    });    
+  });
+  xdescribe("compile", function() {
+    var input = "8*(2+3+5)";
+    var ast = Expression.compile(input);
+    it("should create valid AST", function() {
+      expect(JSON.stringify(ast)).toBe(JSON.stringify({
+        leftNode: {
+          val: 8
+        },
+        op: '*',
+        rightNode: {
+          leftNode: {
+            leftNode: {
+              val: 2
+            },
+            op: '+',
+            rightNode: {
+              val: 3
+            }
+          },
+          op: '+',
+          rightNode: {
+            val: 5
+          }
+        },
+        match: '8*(2+3+5)'
+      }));
+    });
+    it("should create AST that computes right result", function() {
+      expect(ast.result()).toBe(80);
+    });
+    it("should create AST that returns match in root node", function() {
+      expect(ast.match).toBe(input);
+    });
+    xit("should compile and compute result with given variable", function() {
+      var ast = Expression.compile("2+x");
+      expect(ast.result({ x: 1 })).toBe(3);
+      expect(ast.result({ x: 2 })).toBe(4);
+    });
+    xit("should compile and compute with variable appearing more than once", function() {
+      var ast = Expression.compile("x+x*x/2");
+      expect(ast.result({ x: 4 })).toBe(12);
+      expect(ast.result({ x: 8 })).toBe(40);
+    });
+    xit("should compile and compute with more than one variable", function() {
+      var ast = Expression.compile("2584/y-x/3");
+      expect(ast.result({ x: 9, y: 4 })).toBe(643);
+      expect(ast.result({ x: 144, y: 34 })).toBe(28);
+    });
   });
 });
